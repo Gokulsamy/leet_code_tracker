450769a5ed8c7c12275c5b9a587e11d71e9338a5
Gokul anand <151989865+Gokulsamy@users.noreply.github.com>
2026-07-14 14:16:14 +0530

    Create 3206-FindCommonElementsBetweenTwoArrays

---
+++ 3206-FindCommonElementsBetweenTwoArrays/3206-FindCommonElementsBetweenTwoArrays.java
@@ -0,0 +1,29 @@
+// Last updated: 7/14/2026, 2:16:19 PM
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        int c=0;
+        int c1=0;
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    c++;
+                    break;
+
+                }
+                
+            }
+        }
+        for(int i=0;i<nums2.length;i++){
+            for(int j=0;j<nums1.length;j++){
+                if(nums2[i]==nums1[j]){
+                    c1++;
+                    break;
+
+                }
+                
+            }
+        }
+
+        return new int[]{c,c1};
+    }
+}
