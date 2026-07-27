de1ef34f83055d400cbb35967d8df9569f0be509
Gokul anand <151989865+Gokulsamy@users.noreply.github.com>
2026-07-27 11:20:08 +0530

    Create 154-FindMinimuminRotatedSortedArrayII

---
+++ 154-FindMinimuminRotatedSortedArrayII/version/java/154-FindMinimuminRotatedSortedArrayII_20260727_112011.java
@@ -0,0 +1,12 @@
+// Last updated: 7/27/2026, 11:20:11 AM
+1class Solution {
+2    public int findMin(int[] nums) {
+3        int m=nums[0];
+4        for(int i=0;i<nums.length;i++){
+5            if(nums[i]<m){
+6                m=nums[i];
+7            }
+8        }
+9        return m;
+10    }
+11}
