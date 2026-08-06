fcad36cc820e6498178cd59f5cb19a387baeb604
Gokul anand <151989865+Gokulsamy@users.noreply.github.com>
2026-08-06 14:47:19 +0530

    Create

---
+++ problem-1786007839586/version/java/problem-1786007839586_20260806_144719.java
@@ -0,0 +1,19 @@
+// Last updated: 8/6/2026, 2:47:19 PM
+1class Solution {
+2    public String licenseKeyFormatting(String s, int k) {
+3        StringBuilder sb = new StringBuilder();
+4        int count = 0;
+5        for (int i = s.length() - 1; i >= 0; i--) {
+6            char ch = s.charAt(i);
+7            if (Character.isLetterOrDigit(ch)) {
+8                if (count == k) {
+9                    sb.append('-');
+10                    count = 0;
+11                }
+12                sb.append(Character.toUpperCase(ch));
+13                count++;
+14            }
+15        }
+16        return sb.reverse().toString();
+17    }
+18}
